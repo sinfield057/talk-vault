9bbef9fab10e576fd1fa80d215c73e4cce94c9ce
Neagoe Ionut <neagoeionut@Ionuts-MacBook-Pro.local>
2017-10-17 16:00:53 +0300

    Integrated the  variable for Heroku

--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Println("$PORT is not set. Defaulting to 8080...")
+		port = "8080"
+	}
+
 	router := http.NewServeMux()
 	router.HandleFunc("/", testing)
 
-	http.ListenAndServe(":8080", router)
+	http.ListenAndServe(":"+port, router)
 }
 
 func testing(w http.ResponseWriter, r *http.Request) {
